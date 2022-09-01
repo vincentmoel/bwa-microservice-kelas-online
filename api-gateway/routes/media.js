aeb44bdb2a7ec9ef2d3994cde3541e4a2ea9bc9d
Vincent Nathaniel M <vincentmoeljopranoto@gmail.com>
2022-09-02 00:53:26 +0700

    Integrasi Dengan API Gateway Part 2

--- api-gateway/routes/media.js
+++ api-gateway/routes/media.js
@@ -5,4 +5,5 @@ const { APP_NAME } = process.env;
 const mediaHandler = require('./handler/media')
 
 router.post('/', mediaHandler.create); 
+router.get('/', mediaHandler.getAll); 
 module.exports = router;
