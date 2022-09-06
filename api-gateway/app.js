f2bf73b4b03cf609d476996575d9503bb1941773
Vincent Nathaniel M <vincentmoeljopranoto@gmail.com>
2022-09-06 23:27:26 +0700

    Membuat API Refresh Token

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -10,6 +10,8 @@ const coursesRouter     = require('./routes/courses');
 const mediaRouter       = require('./routes/media');
 const ordersRouter      = require('./routes/orders');
 const paymentsRouter    = require('./routes/payments');
+const refreshTokensRouter    = require('./routes/refreshTokens');
+
 
 var app = express();
 
@@ -21,9 +23,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/courses',coursesRouter);
+app.use('/courses', coursesRouter);
 app.use('/media',mediaRouter);
 app.use('/orders',ordersRouter);
 app.use('/payments',paymentsRouter);
+app.use('/refresh-tokens',refreshTokensRouter);
 
 module.exports = app;
