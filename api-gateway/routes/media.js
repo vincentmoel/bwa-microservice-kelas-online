828de4d522369f2d6a08024647500968df27788d
Vincent Nathaniel M <vincentmoeljopranoto@gmail.com>
2022-09-06 18:33:40 +0700

    Integrasi API Login Dan Implementasi JWT

--- api-gateway/routes/media.js
+++ api-gateway/routes/media.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { APP_NAME } = process.env;
 
 const mediaHandler = require('./handler/media')
 
--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -1,10 +1,9 @@
-var express = require('express');
-var router = express.Router();
-const { APP_NAME } = process.env;
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('users');
-});
+const userHandler = require('./handler/users')
+
+router.post('/register', userHandler.register); 
+router.post('/login', userHandler.login); 
 
 module.exports = router;
